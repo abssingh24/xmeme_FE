e4a140b50891229dfa43e9cac36874f5f83e44e9
Abhishek Singh <45766715+abssingh24@users.noreply.github.com>
2021-02-14 22:30:56 +0530

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backEnd_url: "https://sleepy-bastion-54325.herokuapp.com/memes"
 };
 
 /*
